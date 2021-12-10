13c4ccd198b7e97c53238ef27bec5f2727f9cf27
giancarlol <giancarlo.brusca@mercadolibre.com>
2021-12-10 02:17:10 -0300

    add prettier, sass && homepage v1.0.0

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
+import { FaCoffee } from "react-icons/fa";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -12,13 +14,68 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image width={300} height={300} src="/circulo400x400.jpeg" alt="Circulo logo"/>
-        <h1>Próximamente</h1>
+        <Image
+          width={400}
+          height={400}
+          src="/circulo400x400.jpeg"
+          alt="Circulo logo"
+        />
+        <section>
+          <h1>Círculo Vicioso</h1>
+          <h2>
+            Un podcast conducido por{" "}
+            <a
+              href="https://twitter.com/pablowasserman"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Pablo Wasserman
+            </a>{" "}
+            y
+            <a
+              href="https://twitter.com/realjuanruocco"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Juan Ruocco
+            </a>
+          </h2>
+          <h2>Bitcoin, hongos 🍄, monos 🐒 y ranas 🐸.</h2>
+          <a
+            href="https://www.youtube.com/c/CirculoVicioso8"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className={styles.youtubebtn}>
+              <AiFillYoutube />
+            </button>
+          </a>
+          <h3>MIE 21:00h (GMT-3)</h3>
+          <div className={styles.socials}>
+            <a
+              href="https://twitter.com/circulovicioso8"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className={styles.twitter}>
+                <AiOutlineTwitter />
+              </button>
+            </a>
+            <a
+              href="https://cafecito.app/circulovicioso"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className={styles.cafecito}>
+                <FaCoffee />
+              </button>
+            </a>
+          </div>
+        </section>
       </main>
 
-      <footer className={styles.footer}>
-        
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
