62bdd4336be6386fc67bffafb4f37ebd7ae74425
giancarlol <giancarlobrusca@gmail.com>
2022-03-14 04:19:31 -0300

    add _document for font optimization

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Shippori+Antique&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
       <Head>
         <title>Círculo Vicioso Podcast</title>
         <meta name="description" content="Aguante El Círculo papá" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
