fe9df4b3811ad3cddd2e1615ace993678fc45842
giancarlol <giancarlobrusca@gmail.com>
2022-03-14 03:42:44 -0300

    add twitch embed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { TwitchEmbed, TwitchChat, TwitchPlayer } from "react-twitch-embed";
 import { AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
 import { FaCoffee } from "react-icons/fa";
 import styles from "../styles/Home.module.scss";
@@ -14,65 +15,74 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image
-          width={400}
-          height={400}
-          src="/circulo400x400.jpeg"
-          alt="Circulo logo"
+        <TwitchEmbed
+          width="100%"
+          height="100%"
+          channel="circulovicioso8"
+          id="circulovicioso8"
+          theme="dark"
         />
-        <section>
-          <h1>Círculo Vicioso</h1>
-          <h2>
-            Un podcast conducido por{" "}
+        <div>
+          <Image
+            width={400}
+            height={400}
+            src="/circulo400x400.jpeg"
+            alt="Circulo logo"
+          />
+          <section>
+            <h1>Círculo Vicioso</h1>
+            <h2>
+              Un podcast conducido por{" "}
+              <a
+                href="https://twitter.com/pablowasserman"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Pablo Wasserman
+              </a>{" "}
+              y
+              <a
+                href="https://twitter.com/realjuanruocco"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                Juan Ruocco
+              </a>
+            </h2>
+            <h3>Bitcoin, hongos 🍄, monos 🐒 y ranas 🐸.</h3>
             <a
-              href="https://twitter.com/pablowasserman"
+              href="https://www.youtube.com/c/CirculoVicioso8"
               target="_blank"
               rel="noreferrer"
             >
-              Pablo Wasserman
-            </a>{" "}
-            y
-            <a
-              href="https://twitter.com/realjuanruocco"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              Juan Ruocco
-            </a>
-          </h2>
-          <h3>Bitcoin, hongos 🍄, monos 🐒 y ranas 🐸.</h3>
-          <a
-            href="https://www.youtube.com/c/CirculoVicioso8"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className={styles.youtubebtn}>
-              <AiFillYoutube />
-            </button>
-          </a>
-          <h4>MIE 21:00h (GMT-3)</h4>
-          <div className={styles.socials}>
-            <a
-              href="https://twitter.com/circulovicioso8"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className={styles.twitter}>
-                <AiOutlineTwitter />
+              <button className={styles.youtubebtn}>
+                <AiFillYoutube />
               </button>
             </a>
-            <a
-              href="https://cafecito.app/circulovicioso"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className={styles.cafecito}>
-                <FaCoffee />
-              </button>
-            </a>
-          </div>
-        </section>
+            <h4>MIE 21:00h (GMT-3)</h4>
+            <div className={styles.socials}>
+              <a
+                href="https://twitter.com/circulovicioso8"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className={styles.twitter}>
+                  <AiOutlineTwitter />
+                </button>
+              </a>
+              <a
+                href="https://cafecito.app/circulovicioso"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className={styles.cafecito}>
+                  <FaCoffee />
+                </button>
+              </a>
+            </div>
+          </section>
+        </div>
       </main>
 
       <footer className={styles.footer}></footer>
