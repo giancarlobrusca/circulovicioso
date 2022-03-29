3f0ad9c069bbd27717825171761f736e3578cc4a
giancarlol <giancarlobrusca@gmail.com>
2022-03-29 08:13:57 -0300

    add randomclip page and youtube API functionality

---
+++ pages/randomclip/index.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import React from "react";
+import styles from "./clips.module.scss";
+
+export default function Home({ data }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Círculo Vicioso Podcast</title>
+        <meta name="description" content="Aguante El Círculo papá" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.h1}>
+          Tu <i>randomClip</i> del Círculo:
+        </h1>
+        <iframe
+          width="900"
+          height="700"
+          src={`https://www.youtube.com/embed/${
+            data[Math.floor(Math.random() * data.length)].id.videoId
+          }`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
+
+const YOUTUBE_SEARCH_API = "https://www.googleapis.com/youtube/v3/search?";
+const CHANNEL_ID = "UC8yfD9EM8OH_H3HsnR0Szrw";
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    `${YOUTUBE_SEARCH_API}&order=date&maxResults=50&part=snippet&channelId=${CHANNEL_ID}&key=${process.env.YOUTUBE_API_KEY}`,
+    {
+      type: "get",
+    }
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data.items,
+    },
+  };
+}
