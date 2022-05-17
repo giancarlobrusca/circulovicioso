b3561baba49b744175849807fa30ee65fe9685ca
giancarlol <giancarlobrusca@gmail.com>
2022-05-17 03:45:08 -0300

    add google verification metatag

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <meta
+            name="google-site-verification"
+            content="o_S9STYPIw_XSIY00Dhrr8tjSgdAlDyTnIUoan8aD68"
+          />
           <meta name="robots" content="all" />
           <link
             href="https://fonts.googleapis.com/css2?family=Shippori+Antique&display=swap"
