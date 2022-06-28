1ab94b675ab607183ff9f681c01215cb533f287d
giancarlol <giancarlobrusca@gmail.com>
2022-06-27 22:24:07 -0300

    bunny test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import Image from "next/image";
+import { Suspense } from "react";
 import { AiFillYoutube, AiOutlineTwitter } from "react-icons/ai";
 import { FaTiktok } from "react-icons/fa";
 import styles from "../styles/Home.module.scss";
+
+const Bunny = dynamic(() => import("./bunny"), {
+  ssr: false,
+});
 
 export default function Home({ past, live }) {
   function getVideoSrc() {
@@ -18,100 +24,111 @@ export default function Home({ past, live }) {
   }
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Círculo Vicioso Podcast | Bitcoin, hongos, monos y ranas</title>
-        <meta
-          name="description"
-          content="Un podcast conducido por Pablo Wasserman y Juan Ruocco."
-          key="desc"
-        />
-        <meta property="og:title" content="La web oficial de Círculo Vicioso" />
-        <meta
-          property="og:description"
-          content="En vivo los miércoles a las 21:00hs (GMT-3)"
-        />
-        <meta
-          property="og:image"
-          content="https://pbs.twimg.com/profile_banners/1247663322507468800/1591299379/1500x500"
-        />
-      </Head>
-
-      <main className={styles.main}>
-        <iframe
-          width="100%"
-          height="100%"
-          src={getVideoSrc()}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-        <div>
-          <Image
-            width={400}
-            height={400}
-            src="/circulo400x400.jpeg"
-            alt="Circulo logo"
+    <>
+      <div style={{ backgroundColor: "black", height: 1000 }}>
+        <Suspense fallback="Loading...">
+          <Bunny />
+        </Suspense>
+      </div>
+      <div className={styles.container}>
+        <Head>
+          <title>
+            Círculo Vicioso Podcast | Bitcoin, hongos, monos y ranas
+          </title>
+          <meta
+            name="description"
+            content="Un podcast conducido por Pablo Wasserman y Juan Ruocco."
+            key="desc"
           />
-          <section>
-            <h1>Círculo Vicioso</h1>
-            Un podcast conducido por <br />
-            <h2>
+          <meta
+            property="og:title"
+            content="La web oficial de Círculo Vicioso"
+          />
+          <meta
+            property="og:description"
+            content="En vivo los miércoles a las 21:00hs (GMT-3)"
+          />
+          <meta
+            property="og:image"
+            content="https://pbs.twimg.com/profile_banners/1247663322507468800/1591299379/1500x500"
+          />
+        </Head>
+        <main className={styles.main}>
+          <iframe
+            width="100%"
+            height="100%"
+            src={getVideoSrc()}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+          <div>
+            <Image
+              width={400}
+              height={400}
+              src="/circulo400x400.jpeg"
+              alt="Circulo logo"
+            />
+            <section>
+              <h1>Círculo Vicioso</h1>
+              Un podcast conducido por <br />
+              <h2>
+                <a
+                  href="https://twitter.com/pablowasserman"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Pablo Wasserman
+                </a>{" "}
+                y
+                <a
+                  href="https://twitter.com/realjuanruocco"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  Juan Ruocco
+                </a>
+              </h2>
+              <h3>Bitcoin, hongos 🍄, monos 🐒 y ranas 🐸.</h3>
               <a
-                href="https://twitter.com/pablowasserman"
+                href="https://www.youtube.com/c/CirculoVicioso8"
                 target="_blank"
                 rel="noreferrer"
               >
-                Pablo Wasserman
-              </a>{" "}
-              y
-              <a
-                href="https://twitter.com/realjuanruocco"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {" "}
-                Juan Ruocco
-              </a>
-            </h2>
-            <h3>Bitcoin, hongos 🍄, monos 🐒 y ranas 🐸.</h3>
-            <a
-              href="https://www.youtube.com/c/CirculoVicioso8"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className={styles.youtubebtn}>
-                <AiFillYoutube />
-              </button>
-            </a>
-            <h4>MIE 21:00h (GMT-3)</h4>
-            <div className={styles.socials}>
-              <a
-                href="https://twitter.com/circulovicioso8"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className={styles.twitter}>
-                  <AiOutlineTwitter />
+                <button className={styles.youtubebtn}>
+                  <AiFillYoutube />
                 </button>
               </a>
-              <a
-                href="https://www.tiktok.com/@circulovicioso"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className={styles.tiktok}>
-                  <FaTiktok />
-                </button>
-              </a>
-            </div>
-          </section>
-        </div>
-      </main>
+              <h4>MIE 21:00h (GMT-3)</h4>
+              <div className={styles.socials}>
+                <a
+                  href="https://twitter.com/circulovicioso8"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className={styles.twitter}>
+                    <AiOutlineTwitter />
+                  </button>
+                </a>
+                <a
+                  href="https://www.tiktok.com/@circulovicioso"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className={styles.tiktok}>
+                    <FaTiktok />
+                  </button>
+                </a>
+              </div>
+            </section>
+          </div>
+        </main>
 
-      <footer className={styles.footer}></footer>
-    </div>
+        <footer className={styles.footer}></footer>
+      </div>
+    </>
   );
 }
 
