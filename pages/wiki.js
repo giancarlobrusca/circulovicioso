764ae8af693927bfe19e2ad220336179737891ef
giancarlol <giancarlobrusca@gmail.com>
2022-08-26 19:54:00 -0300

    layout + visual fixes

--- pages/wiki.js
+++ pages/wiki.js
@@ -159,31 +159,6 @@ export default function Wiki({ wiki }) {
 
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <Image
-            width={50}
-            height={50}
-            src="/circulo400x400.jpeg"
-            alt="Circulo logo"
-          />
-          <h1>Círculo Vicioso Club</h1>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/wiki">
-                <a>Wiki</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <main className={styles.main}>
         <section>
           <div>
